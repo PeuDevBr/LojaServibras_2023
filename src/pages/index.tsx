1e5fa0f506bba30601e9f1bab003f64ba0e575ef
Flávio Renato <peubevbr@gmail.com>
2023-07-09 10:38:50 -0300

    Add title and icon - V1.02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,44 +3,50 @@ import { Container } from '../styles/pages/home'
 import Link from 'next/link'
 import { useContext } from 'react'
 import { ProductsContext } from '../context/productsContext'
+import Head from 'next/head'
 
 export default function Home() {
   const { productList } = useContext(ProductsContext)
 
   return (
-    <Container>
-      <div className="gridContainer">
-        {productList.map((product) => {
-          return (
-            <div key={product.code} className="productContainer">
-              <div className="productContent">
-                <div className="brandContainer">
-                  <span className="brand">{product.brand}</span>
-                </div>
-                <Image
-                  src={`/images/productsImg/${product.code}.png`}
-                  width={120}
-                  height={120}
-                  className="image"
-                  alt={product.name}
-                />
-                <h2 className="name">{product.name.toUpperCase()}</h2>
-                <h2 className="code">{product.code}</h2>
-                <section>
-                  <button className="productButton" id="add">
-                    ADICIONAR
-                  </button>
-                  <Link href={`/product/${product.code} `} prefetch={false}>
-                    <button className="productButton" id="verify">
-                      VERIFICAR
+    <>
+      <Head>
+        <title>Servibras</title>
+      </Head>
+      <Container>
+        <div className="gridContainer">
+          {productList.map((product) => {
+            return (
+              <div key={product.code} className="productContainer">
+                <div className="productContent">
+                  <div className="brandContainer">
+                    <span className="brand">{product.brand}</span>
+                  </div>
+                  <Image
+                    src={`/images/productsImg/${product.code}.png`}
+                    width={120}
+                    height={120}
+                    className="image"
+                    alt={product.name}
+                  />
+                  <h2 className="name">{product.name.toUpperCase()}</h2>
+                  <h2 className="code">{product.code}</h2>
+                  <section>
+                    <button className="productButton" id="add">
+                      ADICIONAR
                     </button>
-                  </Link>
-                </section>
+                    <Link href={`/product/${product.code} `} prefetch={false}>
+                      <button className="productButton" id="verify">
+                        VERIFICAR
+                      </button>
+                    </Link>
+                  </section>
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </div>
-    </Container>
+            )
+          })}
+        </div>
+      </Container>
+    </>
   )
 }
